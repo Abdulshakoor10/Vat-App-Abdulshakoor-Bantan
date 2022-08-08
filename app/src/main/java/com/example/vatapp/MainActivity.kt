4f0f51db4fcf7ab40cbe275cba45fdaabd895991
Abdulshakoor10 <abdulshakoorbantan@gmail.com>
2022-08-08 08:22:52 +0300

    Binding did't work

---
+++ app/src/main/java/com/example/vatapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.vatapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    // binding object
+//    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        binding = ActivityMainBin
+    }
+
+}
